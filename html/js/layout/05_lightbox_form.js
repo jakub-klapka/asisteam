c674e214f19b81f67baf7c57446811ccd3c96b92
Jakub Klapka <klapka@lumiart.cz>
2014-05-22 19:45:12 +0200

    Initial iframe setup

---
+++ html/js/layout/05_lightbox_form.js
@@ -0,0 +1,18 @@
+/* global jQuery */
+(function($){
+
+	var LumiLightboxForm = {
+
+		init: function() {
+
+
+
+		}
+
+	};
+
+	$(document).ready(function(){
+		LumiLightboxForm.init();
+	});
+
+})(jQuery);
